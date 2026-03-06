50390ca381c0d5b888d9ded70767dee745c244ba
tinu <tinutoms55@gmail.com>
2026-03-06 20:58:16 +0530

    new-python4

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-a=int(input("enter the first no:"))
-b=int(input("enter the second no:"))
-d=int(input("enter third no:"))
-
-c=a+b+d
+print("enter first no:")
+read a
+print("enter second no:")
+read b
+c=a+b
 print(c)
-
